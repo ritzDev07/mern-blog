d1729ac591782345735572f9c8c75932d4d07fad
ritzDev07 <ritzocite@gmail.com>
2024-02-02 10:41:33 +0800

    Create Pages & Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,54 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
 
 const App = () => {
 	return (
-		<div>
-			<h1 className=" bg-slate-400">
-				Client Initial layout
-			</h1>
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<Home />
+					}
+				/>
+				<Route
+					path="/about"
+					element={
+						<About />
+					}
+				/>
+				<Route
+					path="/sign-in"
+					element={
+						<SignIn />
+					}
+				/>
+				<Route
+					path="/sign-up"
+					element={
+						<SignUp />
+					}
+				/>
+				<Route
+					path="/dashboard"
+					element={
+						<Dashboard />
+					}
+				/>
+				<Route
+					path="/projects"
+					element={
+						<Projects />
+					}
+				/>
+			</Routes>
+		</BrowserRouter>
 	)
-}
+};
 
-export default App
+export default App;
